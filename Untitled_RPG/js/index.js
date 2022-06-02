c13944e5731ec0a7929e75efe964d31495393b4a
Azriel Policarpio <52266762+Ikusey@users.noreply.github.com>
2022-06-01 22:57:53 -0700

    Week 4 updates
    added in enemy descriptor and more enemies, some of which can attack twice

--- Untitled_RPG/js/index.js
+++ Untitled_RPG/js/index.js
@@ -9,22 +9,52 @@ let strengthE = 0, defenseE = 0, constitutionE = 0
 let points = 3
 let coins = 0
 let healthPots = 0
-const Goblin = {name: "Goblin", strength: 0, defense: 0, constitution: 0}
-const Bandit = {name: "Bandit", strength: 1, defense: 1, constitution: 1}
-const Orc = {name: "Orc", strength: 2, defense: 2, constitution: 2}
-const Troll = {name: "Troll", strength: 3, defense: 3, constitution: 3}
-const Ogre = {name: "Ogre", strength: 4, defense: 4, constitution: 4}
-const enemies = [Goblin, Bandit, Orc, Troll, Ogre]
+const Goblin = {name: "Goblin", strength: 0, defense: 0, constitution: 0, description: 
+"Goblins are small, black-hearted humanoids that lair in despoiled dungeons and other dismal settings. Individually weak, they gather in large numbers to torment other creatures."}
+
+const Bandit = {name: "Bandit", strength: 1, defense: 1, constitution: 1, description:
+"Bandits rove in gangs and are sometimes led by thugs, veterans, or spellcasters. Not all bandits are evil. Oppression, drought, disease, or famine can often drive otherwise honest folk to a life of banditry. Pirates are bandits of the high seas. They might be freebooters interested only in treasure and murder, or they might be privateers sanctioned by the crown to attack and plunder an enemy nation's vessels."}
+
+const Orc = {name: "Orc", strength: 2, defense: 2, constitution: 2, description:
+"Orcs are savage humanoids with stooped postures, piggish faces, and prominent teeth that resemble tusks. They gather in tribes that satisfy their bloodlust by slaying any humanoids that stand against them."}
+
+const Troll = {name: "Troll", strength: 3, defense: 3, constitution: 3, description:
+"Fearsome green-skinned giants, trolls eat anything they can catch and devour. Only acid and fire can arrest the regenerative properties of a troll’s flesh."}
+
+const Ogre = {name: "Ogre", strength: 4, defense: 4, constitution: 4, description:
+"Ogres are hulking giants notorious for their quick tempers. When its rage is incited, an ogre lashes out in a frustrated tantrum until it runs out of objects or creatures to smash."}
+
+const Skeleton = {name: "Skeleton", strength: 2, defense: -1, constitution: -1, description:
+"Skeletons are the re-animated, completely decomposed body of a humanoid. The flesh has completely disappeared, leaving a bone structure magically held together, that moves oddly in its attempt to imitate life"}
+
+const Zombie = {name: "Zombie", strength: 1, defense: 3, constitution: 3, description: 
+"Undead zombies move with a jerky, uneven gait. They are clad in the moldering apparel they wore when put to rest, and carry the stench of decay."}
+
+const Kobold = {name: "Kobold", strength: -1, defense: -1, constitution: -1, description: 
+"Kobolds are often dismissed as cowardly, foolish, and weak, but these little reptilian creatures actually have a strong social structure that stresses devotion to the tribe, are clever with their hands, and viciously work together in order to overcome their physical limitations. Can attack twice."}
+
+const Harpie = {name: "Harpie", strength: 1, defense: 1, constitution: 1, description: 
+"A harpy combines the body, legs, and wings of a vulture with the torso, arms, and head of a human female. Its sweet song has lured countless adventurers to their deaths. Can attack twice."}
+
+const enemies = [Goblin, Bandit, Orc, Troll, Ogre, Skeleton, Zombie, Kobold, Harpie]
 let currentEnemy = Goblin.name
 let hasSword = false
 let hasShield = false
 let hasArmor = false
+let initialize = 0
 
  function attack(){
+     if(initialize = 0){
+       newFight()
+       initialize++
+     }
      x = Math.floor((Math.random() * 10) + 1) + strength - currentEnemy.defense
      b = Math.floor((Math.random() * 10) + 1) - defense + currentEnemy.strength
      y = y - b
      a = a - x
+     if(currentEnemy.name == "Harpie" || currentEnemy.name == "Kobold"){
+       y = y - b
+     }
      result()
      if(countS > 0){
          countS--
@@ -73,10 +103,10 @@ let hasArmor = false
   }
 
  function newFight(){
-    currentEnemy = enemies[Math.floor((Math.random() * 4))]
+    currentEnemy = enemies[Math.floor((Math.random() *8))]
     x = 0
-    y = 100 + constitution
-    a = 100 + currentEnemy.constitution
+    y = 100 + (constitution * 10)
+    a = 100 + (currentEnemy.constitution * 10)
     b = 0
     countS = 0
     countH = 0
@@ -200,7 +230,7 @@ function useHealthPot(){
      document.getElementById("strengthE").innerHTML = currentEnemy.strength
      document.getElementById("defenseE").innerHTML = currentEnemy.defense
      document.getElementById("constitutionE").innerHTML = currentEnemy.constitution
-     
+     document.getElementById("enemyDescription").innerHTML = currentEnemy.description
      if(a <= 0 && y > 0){
       alert("You win!")
       points++
